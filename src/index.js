7515a406d0b33fc97be37327a29a30bff47ffb4d
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-03-10 17:39:31 +0200

    решение

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import './styles.css';
 import './js/main';
-
-import menuElement from './js/menu.json';
-import cardTmpl from './templates/card.hbs';
-
-let menu = document.querySelector('.js-menu');
-
-function addMarkupMenu(arr) {
-  const markup = arr.map(card => cardTmpl(card)).join('');
-  menu.insertAdjacentHTML('beforeend', markup);
-}
-addMarkupMenu(menuElement);
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
+import menuElement from '../js/menu.json';
+import cardTmpl from '../templates/card.hbs';
+
 const refs = {
   toggle: document.querySelector('#theme-switch-toggle'),
   body: document.querySelector('body'),
 };
+
+let menu = document.querySelector('.js-menu');
+
+function addMarkupMenu(arr) {
+  const markup = arr.map(card => cardTmpl(card)).join('');
+  menu.insertAdjacentHTML('beforeend', markup);
+}
+addMarkupMenu(menuElement);
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -14,8 +25,18 @@ function changeTheam(e) {
   if (e.target.checked) {
     refs.body.classList.add(Theme.DARK);
     refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
   } else {
     refs.body.classList.add(Theme.LIGHT);
     refs.body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
   }
+}
+
+const localStorageThem = localStorage.getItem('theme', Theme.LIGHT);
+if (localStorageThem === Theme.DARK) {
+  refs.toggle.checked = true;
+  refs.body.classList.add(Theme.DARK);
+} else {
+  refs.body.classList.add(Theme.LIGHT);
 }
